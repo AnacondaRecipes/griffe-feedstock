22188981651cd2dc4d60aa989715900ba120ba86
xkong-anaconda <xkong@anaconda.com>
2026-05-04 13:38:48 -0500

    Use inline build.script per output (with [unix]/[win] selectors); remove .bat/.sh files

--- recipe/bld-griffecli.bat
+++
@@ -1,4 +0,0 @@
-set UV_DYNAMIC_VERSIONING_BYPASS=%PKG_VERSION%
-if errorlevel 1 exit 1
-%PYTHON% -m pip install .\griffecli --no-deps --no-build-isolation -vv
-if errorlevel 1 exit 1
--- recipe/build-griffe.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -ex
-export UV_DYNAMIC_VERSIONING_BYPASS="${PKG_VERSION}"
-"${PYTHON}" -m pip install ./griffe --no-deps --no-build-isolation -vv
